ec08f9ec4ba7dd12d5536ec6d4a3793ec50d641b
Caleb Penning <calebpenning@mail.com>
2023-03-18 15:03:19 -0700

    make feature available in all servers bot belongs to

--- src/deploy-command.ts
+++ src/deploy-command.ts
@@ -29,7 +29,7 @@ const rest = new REST({ version: "10" }).setToken(token)
 		)
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{
 				body: commands,
 			}
