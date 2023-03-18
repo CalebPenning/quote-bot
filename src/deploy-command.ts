af9434ac56bd1115a7fe57e4fb238e315cfb5c84
Caleb Penning <calebpenning@mail.com>
2023-03-18 15:10:08 -0700

    add new script to rebuild dist folder before npm start, change from app guild command to app command

--- src/deploy-command.ts
+++ src/deploy-command.ts
@@ -6,7 +6,6 @@ import path from "path"
 dotenv.config()
 const clientId = process.env.DISCORD_CLIENT_ID as string
 const token = process.env.DISCORD_TOKEN as string
-const guildId = process.env.DISCORD_GUILD_ID as string
 
 const commands = []
 
@@ -28,12 +27,9 @@ const rest = new REST({ version: "10" }).setToken(token)
 			`Started refreshing ${commands.length} application (/) commands.`
 		)
 
-		const data = await rest.put(
-			Routes.applicationCommands(clientId),
-			{
-				body: commands,
-			}
-		)
+		const data = await rest.put(Routes.applicationCommands(clientId), {
+			body: commands,
+		})
 
 		console.log(
 			`Successfully reloaded ${
