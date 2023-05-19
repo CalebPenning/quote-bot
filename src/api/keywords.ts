615fe0cf9a5b1076de36ae538ad18b8fbc3e695f
Caleb Penning <calebpenning@mail.com>
2023-05-19 10:08:11 -0700

    add try/catch blocks around api calls that fail

--- src/api/keywords.ts
+++ src/api/keywords.ts
@@ -5,6 +5,11 @@ dotenv.config()
 const apiBaseUrl = process.env.API_BASE_URL as string
 
 export default async function getAllKeywords() {
+	try {
 	const { data } = await axios.get(`${apiBaseUrl}/keywords`)
-	return data
+	return data 
+	}
+	catch(e) {
+		console.error(e)
+	}
 }
--- src/api/quotes.ts
+++ src/api/quotes.ts
@@ -5,6 +5,10 @@ dotenv.config()
 const apiBaseUrl = process.env.API_BASE_URL as string
 
 export default async function getAllQuotes() {
+	try {
 	const { data } = await axios.get(`${apiBaseUrl}/quotes`)
 	return data
+	} catch(e) {
+		console.error(e)
+	}
 }
